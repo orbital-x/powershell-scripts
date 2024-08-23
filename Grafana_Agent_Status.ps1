d49f694a2d77c842174f9b15b4e6fc20266dcdb5
obseedian <104126259+orbital-x@users.noreply.github.com>
2024-08-22 18:09:59 -0700

    Add files via upload

---
+++ Grafana_Agent_Status.ps1
@@ -0,0 +1,10 @@
+﻿$GrafanaService = Get-WmiObject -Class Win32_Service -Filter "Name='grafana-agent'" 
+
+if (!($GrafanaService)) {$RadiaGrafanaDir = Test-Path C:\install\PkgSrc\WIN_GRAFANA_AGENT
+
+                         If(!($RadiaGrafanaDir)) {Write-Host "Grafana Agent Missing \ PkgSrc Missing"}
+                         Else {Write-Host "Grafana Agent Missing \ PkgSrc Exists"}
+
+                         }
+
+else {(Get-Service -Name grafana-agent).status}
---
+++ Grafana_RESTART_Service.ps1
@@ -0,0 +1,18 @@
+﻿$GrafanaService = Get-WmiObject -Class Win32_Service -Filter "Name='grafana-agent'" 
+
+if (!($GrafanaService)) {write-host "Grafana Agent Missing"} 
+
+else {Stop-Service -Name grafana-agent
+      sleep 5
+      Start-Service -Name  grafana-agent
+      sleep 5
+
+      $GrafanaState = ($GrafanaService).State
+
+        If($GrafanaState -ne 'Running') {Write-Host "Service Failed to Start"}
+                                        
+        Else {Write-Host "Service restart successfull"}
+
+      }
+
+
