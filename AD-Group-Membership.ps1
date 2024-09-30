47e8fbd58a617ac9542857eb339c22f140f073c4
Francisco Perez <fpere47@safeway.com>
2024-09-30 11:20:56 -0700

    modified ad-group-membership to remove errors

--- AD-Group-Membership.ps1
+++ AD-Group-Membership.ps1
@@ -1,10 +1,10 @@
 # USER-memebership
 # The Get-ADPrincipalGroupMembership cmdlet gets the Active Directory groups that have a specified user, computer, group, or service account as a member. 
 # This cmdlet requires a global catalog to perform the group search.  
-Get-ADPrincipalGroupMembership <UserID> | sort-object | Select-Object name 
+Get-ADPrincipalGroupMembership UserID | sort-object | Select-Object name 
 
  
 #COMPUTER-Memebership
-Get-ADPrincipalGroupMembership (Get-ADComputer hostname).DistinguishedName | select-object samaccountname 
+Get-ADPrincipalGroupMembership (Get-ADComputer computername).DistinguishedName | select-object samaccountname 
 
 
