b0fc379d96e0c053dae87978ff7159a17ecd7a24
Francisco Perez <fpere47@safeway.com>
2024-10-08 09:41:49 -0700

    Updated Performance-monitor script

--- COMMANDS/Performance-Monitor-MSOFT.ps1
+++ COMMANDS/Performance-Monitor-MSOFT.ps1
@@ -1,9 +1,9 @@
 # Run as Administrator in CMD: 
-Logman.exe create counter 1secinterval -f bincirc -v mmddhhmm -max 512 -c "\LogicalDisk(*)\*" "\Memory\*" "\Cache\*" "\Network Interface(*)\*" "\Paging File(*)\*" "\PhysicalDisk(*)\*" "\Processor(*)\*" "\Processor Information(*)\*" "\Process(*)\*" "\Redirector\*" "\Server\*" "\System\*" "\Server Work Queues(*)\*" "\Terminal Services\*" -si 00:00:05 
+Logman.exe create counter 5secinterval -f bincirc -v mmddhhmm -max 512 -c "\LogicalDisk(*)\*" "\Memory\*" "\Cache\*" "\Network Interface(*)\*" "\Paging File(*)\*" "\PhysicalDisk(*)\*" "\Processor(*)\*" "\Processor Information(*)\*" "\Process(*)\*" "\Redirector\*" "\Server\*" "\System\*" "\Server Work Queues(*)\*" "\Terminal Services\*" -si 00:00:05 
 
 # You can then start and stop the log with the following commands: 
-logman start 1secinterval  
-logman stop 1secinterval 
+logman start 5secinterval  
+logman stop 5secinterval 
 
  
 # PAL2 PerfMon Capture Analyzer Tool: 
