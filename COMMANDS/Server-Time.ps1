13f366f97c0b921fc4ef253612f9d9041078403f
Francisco Perez <fpere47@safeway.com>
2024-10-08 17:48:22 -0700

    created server-time.ps1

---
+++ COMMANDS/Server-Time.ps1
@@ -0,0 +1,20 @@
+# Start/stop  time service 
+net stop w32time 
+net start w32time 
+
+# Resync the time 
+w32tm /resync 
+
+# Verify your sync status: 
+w32tm /query /status 
+
+# ------------------------------- 
+
+# List Peers
+w32tm /query /peers 
+
+# List out NTP Sources: 
+w32tm /query /source 
+
+# To update, use the command below (2008 and 2012 server compatible): 
+w32tm /config /manualpeerlist:"ntp_server" /syncfromflags:manual /reliable:yes /update 
