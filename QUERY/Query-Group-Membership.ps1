481bdeed4729a1051413ed6f565d00ab20c8660d
Francisco Perez <fpere47@safeway.com>
2025-02-21 12:51:45 -0700

    deleted duplicate Ad-group-memeber script

--- QUERY/Query-Group-Membership.ps1
+++ QUERY/Query-Group-Membership.ps1
@@ -1,21 +1,26 @@
-<#
+<#================================================================================================================================
+==================================================================================================================================
     .NOTES
         Author: Roboute Guilliman
         Created: 09/2024
 
     .SYNOPSIS
-        Queries Active Directory for a user or computer's group membership
+        -Queries security group memebership for AD User or Computer in Active Directory
+        -Queries Network folder scurity permissions
+        -Queries local computer's security memebership for a specific group
 
     .DESCRIPTION
         -The Get-ADPrincipalGroupMembership cmdlet gets the Active Directory groups that have a specified user, computer, group, or service account as a member. 
-        -This cmdlet requires a global catalog to perform the group search. 
-#>
+        -This cmdlet requires a global catalog to perform the group search.
+
+=================================================================================================================================
+================================================================================================================================#>
 
 
-# AD-USER-memebership
+# ACTIVE-DIRECTORY-USER-memebership
 Get-ADPrincipalGroupMembership UserID | sort-object | Select-Object name 
 
-#AD-COMPUTER-Memebership
+# ACTIVE-DIRECTORY-COMPUTER-Memebership
 Get-ADPrincipalGroupMembership (Get-ADComputer computername).DistinguishedName | select-object samaccountname 
 
 
@@ -26,5 +31,5 @@ $TargetFolder = "\\folder\directory"
 
 
 #-------------------------------------------------------------------------------------------------------------
-#LOCAL-Group-Membership
+# LOCAL-Group-Membership
 Get-LocalGroupMember -Group "Administrators"
